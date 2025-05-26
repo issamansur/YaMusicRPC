922741c33302f7d955e3439ed63c4118ad077b3f
User#None <77580844+issamansur@users.noreply.github.com>
2025-05-26 18:53:47 +0300

    Fix build options

--- utils/build_pyinstaller_mac.sh
+++ utils/build_pyinstaller_mac.sh
@@ -6,6 +6,9 @@ pyinstaller application/YaMusicRPC.py \
   --noconsole \
   --name "YaMusicRPC" \
   --icon=application/resources/logo.png \
-  --add-data "application/resources/logo.png:resources"
+  --add-data "application/resources/logo.png:resources" \
+  --add-data "yamusicrpc/server/callback.html:yamusicrpc/server"
+  # WE INCLUDE CALLBACK.HTML FROM LOCAL!!!
+  # Another variant - use --collect-all yamusicrpc (but it include all files .py, even if they no needed)
 
 echo "Build completed!"
